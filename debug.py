8babff7726f56de304b4fc2350c7c2653b557dd6
Warths <pro.warths@gmail.com>
2020-10-21 12:55:47 +0200

    Added a debugger

---
+++ debug.py
@@ -0,0 +1,18 @@
+import time
+from logger import log
+
+class Debugger:
+    def __init__(self, debug, interval):
+        self.debug = debug
+        self.interval = interval
+
+        self.last_log = 0
+
+    def log(self, message):
+        if self.debug:
+            log.discrete("[DEBUG] " + message)
+
+    def log_on_interval(self, message):
+        if self.last_log + self.interval < time.time():
+            self.log(message)
+            self.last_log = time.time()
